61bbd5d7071af55fa98a418cbcbd6b2ee04f1aeb
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-05-02 20:09:32 -0700

    Added/Registered the Entry Model

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Entry
 # Register your models here.
 
 admin.site.register(Topic)
+admin.site.register(Entry)
 
 #it imports the model we want to register, Topic.
 #the dot in front of models tells Django to look for models.py in the same directory as admin.py
