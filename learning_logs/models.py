4f175b311ee146cc4b80c005111abc99ae8c8cbf
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-05-02 18:11:09 -0700

    Added model for Entry from user and made migration to database, Also updated the gitignore

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # We will define the data we want to manage in our app
 # We(user) will create multiple topic in learning log and and the entry they will make will be tied to a topic
 # We will also store timestamp of each entry
-#Hello
+
 
 
 class Topic(models.Model):
@@ -20,3 +20,15 @@ class Topic(models.Model):
     If a model has a __str__() method, 
     Django calls that method whenever it needs to generate output referring to an instance of that model
     """
+
+class Entry(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+    
+    def __str__(self):
+        "Returning simple string representing the entry"
+        return f"{self.text[:50]}..."
