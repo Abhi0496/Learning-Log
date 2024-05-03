42884edd8e6859c9c98b6028145bf390cda9c44e
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-05-02 20:10:59 -0700

    Added condition in Entry to add ellipsis only if text length > 50

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -31,4 +31,7 @@ class Entry(models.Model):
     
     def __str__(self):
         "Returning simple string representing the entry"
-        return f"{self.text[:50]}..."
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
