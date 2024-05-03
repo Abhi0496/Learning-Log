8ad7eb7eccda50a73c0572dde57e59dde0eb05f1
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-05-02 22:09:06 -0700

    Created view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+#it takes info from requests, prepares the data needed to generate a page, and then sends the data back to the browser
+
+def index(request):
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
