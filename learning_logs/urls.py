60a20ee448debaa887d65693c4ab5800084fccf9
Abhishek Agrawal <aaagrawal.abhishek@gmail.com>
2024-05-04 01:22:19 -0700

    Added url for topic page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -18,4 +18,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     #Path to show all topics.
     path('topics/', views.topics, name='topics'),
+    #Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
